a3bd1e7e8ae31f697d0fc0965bd0ef93ff0d8736
MCerqua <55824402+MCERQUA@users.noreply.github.com>
2025-02-05 14:36:01 -0500

    Update: 2025-02-05 14:36:01.20

--- GIT-UPDATE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git init
-git remote remove origin
-git remote add origin https://github.com/MCERQUA/ECHO1.git
-git branch -M main
-git add .
-git commit -m "Update"
-git push -u origin main --force
-pause
